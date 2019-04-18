8adbf52b4a9e9a44832e96b675fa2417c3ee35f7
Josef Klafka <jklafka@diglett.cs.uchicago.edu>
2019-04-18 18:57:47 -0500

    started on making more complex stimuli

--- past-tense/complex_data_construction.py
+++ past-tense/complex_data_construction.py
@@ -9,19 +9,41 @@ with open("data/all_verbs.csv", 'r') as f:
          verbs.append(row[0])
 verbs = verbs[1:]
 
-subjects = [('I', 0), ('We', 1), ('It', 0), ('They', 1), ('Person', 0), \
-    ('People', 1), ('Man', 0), ('Men', 1), ('Woman', 0), ('Women', 1), \
-    ('Goose', 0), ('Geese', 1), ('Mouse', 0), ('Mice', 1), ('Ox', 0), \
-    ('Oxen', 1)]
+trainVerbs = verbs[:401]
 
-adjuncts = ["in my house", "with my father", "with my fathers", "in our house"]
+testVerbs = verbs[401:]
+
+Pronouns = [('I', 0), ('We', 1), ('It', 0), ('They', 1)]
+
+trainNouns = [('Person', 0), ('People', 1), ('Man', 0), ('Men', 1),
+    ('Woman', 0), ('Women', 1), ("Linguist", 0), ("Linguists", 1) \
+    ('Goose', 0), ('Geese', 1), ('Mouse', 0), ('Mice', 1), ('Ox', 0), \
+    ('Oxen', 1), ('Sloth', 0), ('Sloths', 1), ('Narwhal', 0), ('Narwhals', 1), \
+    ('Judge', 0), ('Judges', 1), ('Engineer', 0), ('Engineers', 1)]
+
+testNouns = [('Dog', 0), ('Dogs', 1), ('Cat', 0), \
+    ('Cats', 1), ('Bird', 0), ('Birds', 1), ('Friend', 0), ('Friends', 1), \
+    ('Savior', 0), ('Saviors', 1), ('Doctor', 0), ('Doctors', 1)]
+
+subjectRCs = [""]
+
+objectRCs = [""]
+
+itClefts = ["It was the {} that"]
+
+negAdjuncts = ["but not the {}", "without the {}"]
+
+embedClauses = ["that we found", "that I found", "that she created", \
+    "that they created"]
+
+Adjuncts = ["in my house", "with my father", "with my fathers", "in our house"]
 
 sens = []
 
-for item in subjects:
-     for verb in verbs:
-         j = random.randint(0, len(adjuncts) - 1)
-         sens.append((item[0] + ' ' + verb + ' ' + adjuncts[j], item[1]))
+# for item in subjects:
+#      for verb in verbs:
+#          j = random.randint(0, len(adjuncts) - 1)
+#          sens.append((item[0] + ' ' + verb + ' ' + adjuncts[j], item[1]))
 
 random.shuffle(sens)
 
