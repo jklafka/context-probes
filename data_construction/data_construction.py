786c847ecbea88ea4d4db17604373bec7509cee5
Academic <academic@Ancalagon.local>
2019-07-31 09:23:18 -0500

    analysis pipeline

--- data_construction/data_construction.py
+++ data_construction/data_construction.py
@@ -19,19 +19,24 @@ testSubjs = [('lackey', 0), ('lackeys', 1), ('boss', 0), \
     ('bosses', 1), ('kid', 0), ('kids', 1), ('friend', 0), \
     ('friends', 1), ('savior', 0), ('saviors', 1), ('doctor', 0), ('doctors', 1)]
 
+trainAdj = ["friendly", "helpful", "old", "young", "new"]
+testAdj = ["good", "bad", "angry", "strange", "normal"]
+
 sens = []
 
 for trainVerb in trainVerbs:
     for trainSubj in trainSubjs:
         for trainObj in trainSubjs:
             # if trainSubj[1] != trainObj[1]:
-                sens.append(("The " + trainSubj[0] + ' ' + trainVerb + ' ' + trainObj[0] \
-                + ' the', trainObj[1]))
+                sens.append(("The " + trainSubj[0] + ' ' + trainVerb + ' the ' + \
+                + trainObj[0], trainObj[1]))
                 # sens.append(("The " + trainObj[0] + " was " + trainVerb + " by the " \
                 #                 + trainSubj[0], trainSubj[1]))
+                sens.append(("The " + ))
 random.shuffle(sens)
+sens = sens[:4000]
 
-with open("../data/flip_obj_train.csv", 'w') as csv_file:
+with open("../data/distance_train.csv", 'w') as csv_file:
      data_writer = csv.writer(csv_file, delimiter = ',')
      for row in sens:
          data_writer.writerow(list(row))
@@ -42,13 +47,14 @@ for testVerb in testVerbs:
     for testSubj in testSubjs:
         for testObj in testSubjs:
             # if testSubj[1] != testObj[1]:
-                sens.append(("The " + testSubj[0] + ' ' + testVerb + ' ' + testObj[0] \
-                            + ' the', testObj[1]))
+                sens.append(("The " + testSubj[0] + ' ' + testVerb + ' the ' \
+                + testObj[0], testObj[1]))
                 # sens.append(("The " + testObj[0] + " was " + testVerb + " by the " \
                 #                 + testSubj[0], testSubj[1]))
 random.shuffle(sens)
+sens = sens[:1000]
 
-with open("../data/flip_obj_test.csv", 'w') as csv_file:
+with open("../data/distance_test.csv", 'w') as csv_file:
      data_writer = csv.writer(csv_file, delimiter = ',')
      for row in sens:
          data_writer.writerow(list(row))
