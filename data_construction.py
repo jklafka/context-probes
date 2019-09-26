b19db2a6f4c47fbec7f6fe59c65f1bbd78302e0b
Josef Klafka <jlklafka@gmail.com>
2019-09-26 17:24:16 -0400

    new data generation procedure for non-wc probes

--- data_construction.py
+++ data_construction.py
@@ -1,46 +1,74 @@
 import csv
 import random
 
-subjects = []
+nouns = []
+
+# change this to whatever you want the name of the output files to be
+output_name = "verb"
+# change the target for the noun tasks
+target_name = "object"
 
 ## number
-with open("stimuli/number_nouns.csv", 'r') as f:
-     reader = csv.reader(f)
-     for line in reader:
-         subjects.append((line[0], 0))
-         subjects.append((line[1], 1))
+if output_name == "number":
+    with open("stimuli/number_nouns.csv", 'r') as f:
+         reader = csv.reader(f)
+         for line in reader:
+             nouns.append((line[0], 0))
+             nouns.append((line[1], 1))
 
 ## gender
-with open("stimuli/gender_nouns.csv", 'r') as f:
-     reader = csv.reader(f)
-     for line in reader:
-         subjects.append((line[0], int(line[1])))
+if output_name == "gender":
+    with open("stimuli/gender_nouns.csv", 'r') as f:
+         reader = csv.reader(f)
+         for line in reader:
+             nouns.append((line[0], int(line[1])))
 
 verbs = open("stimuli/clean_verbs.txt", 'r').read().split()
-trainVerbs = verbs[:80]
-testVerbs = verbs[80:100]
 
 ## tense
-# present_verbs = open("stimuli/present_verbs.txt", 'r').read().split()
+if output_name == "tense":
+    present_verbs = open("stimuli/present_verbs.txt", 'r').read().split()
+    trainVerbs = list(zip(verbs[:40], [1] * 40)) + \
+        list(zip(present_verbs[:40], [0] * 40))
+    testVerbs = list(zip(verbs[81:90], [1] * 10)) + \
+        list(zip(present_verbs[81:90], [0] * 10))
+    # get subjects and objects
+    with open("stimuli/other_nouns.csv", 'r') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            nouns.append((line[0],'a'))
 
-## alternate nouns
-alternates = []
-with open("stimuli/other_nouns.csv", 'r') as f:
-    reader = csv.reader(f)
-    for line in reader:
-        alternates.append(line[0])
+else:
+    trainVerbs = list(zip(verbs[:80], ['a']*80))
+    testVerbs = list(zip(verbs[80:100], ['a']*80))
+
+# create train/test split with subjects
+trainSubjs = nouns[:40]
+testSubjs = nouns[41:50]
+
 
 sens = []
 
 for trainVerb in trainVerbs:
     for trainSubj in trainSubjs:
         for trainObj in trainSubjs:
-            ## stuff goes here
+            if trainObj != trainSubj:
+                if output_name == "tense":
+                    target = trainVerb
+                else:
+                    if target_name == "subject":
+                        target = trainSubj
+                    elif target_name == "object":
+                        target = trainObj
+                    else:
+                        break
+                sens.append(("the " + trainSubj[0] + ' ' + trainVerb[0] + " the " + \
+                    trainObj[0], target[1]))
 
 random.shuffle(sens)
 sens = sens[:4000]
 
-with open("../data/quant_train.csv", 'w') as csv_file:
+with open("data/" + target_name + '_' + output_name + "_train.csv", 'w') as csv_file:
      data_writer = csv.writer(csv_file, delimiter = ',')
      for row in sens:
          data_writer.writerow(list(row))
@@ -50,13 +78,24 @@ sens = []
 for testVerb in testVerbs:
     for testSubj in testSubjs:
         for testObj in testSubjs:
-            ## stuff goes here
+            if testObj != testSubj:
+                if output_name == "tense":
+                    target = testVerb
+                else:
+                    if target_name == "subject":
+                        target = testSubj
+                    elif target_name == "object":
+                        target = testObj
+                    else:
+                        break
+                sens.append(("the " + testSubj[0] + ' ' + testVerb[0] + " the " + \
+                    testObj[0], target[1]))
 
 
 random.shuffle(sens)
 sens = sens[:1000]
 
-with open("../data/quant_test.csv", 'w') as csv_file:
+with open("data/" + target_name + '_' + output_name + "_test.csv", 'w') as csv_file:
      data_writer = csv.writer(csv_file, delimiter = ',')
      for row in sens:
          data_writer.writerow(list(row))
