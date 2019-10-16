0ec4bef1118a2a8038679f90ff3f68947812d0c0
Josef Klafka <jlklafka@gmail.com>
2019-10-16 15:32:56 -0400

    create kway classification construction and classifier

---
+++ classifiers/kway_classifier.py
@@ -0,0 +1,108 @@
+import torch, csv, logging, argparse
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+## set up logging
+parser = argparse.ArgumentParser()
+parser.add_argument("logname")
+args = parser.parse_args()
+logging.basicConfig(filename = "results/" + args.logname + ".csv", format="%(message)s", level=logging.INFO)
+
+# get CUDA device
+device = torch.device("cuda:0")
+
+## define neural networks: first has three hidden layers
+class ThreeLayerNet(nn.Module):
+    def __init__(self, input_dim, hidden_dims):
+        super().__init__()
+        self.lin1 = nn.Linear(input_dim, hidden_dims[0], bias = True)
+        self.lin2 = nn.Linear(hidden_dims[0], hidden_dims[1], bias = True)
+        self.lin3 = nn.Linear(hidden_dims[1], hidden_dims[2], bias = True)
+        self.linout = nn.Linear(hidden_dims[2], 20)
+
+    def forward(self, x):
+        output = F.relu(self.lin1(x))
+        output = F.relu(self.lin2(output))
+        output = F.relu(self.lin3(output))
+        output = F.relu(self.linout(output))
+        return output
+
+## this neural net has only one hidden layer
+class OneLayerNet(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        self.lin1 = nn.Linear(input_dim, 1024, bias = True)
+        self.linout = nn.Linear(1024, 20)
+
+    def forward(self, x):
+        output = F.relu(self.lin1(x))
+        output = F.relu(self.linout(output))
+        return output
+
+# record the different reps we're using
+REPS = ["bert", "elmo", "gpt"]
+
+
+for rep in REPS:
+    # for each word
+    for i in range(3):
+        trainset = []
+        trainlabels = []
+        with open("data/" + rep + "/train" + str(i) + ".csv", 'r') as train:
+            reader = csv.reader(train)
+            for row in reader:
+                trainset.append(list(map(float, row[:-1])))
+                trainlabels.append(row[-1])
+        trainset = torch.FloatTensor(trainset).to(device)
+        trainlabels = [int(label) for label in trainlabels]
+        trainlabels = torch.LongTensor(trainlabels).to(device)
+
+        testset = []
+        testlabels = []
+        with open("data/" + rep + "/test" + str(i) + ".csv", 'r') as test:
+            reader = csv.reader(test)
+            for row in reader:
+                testset.append(list(map(float, row[:-1])))
+                testlabels.append(row[-1])
+        testset = torch.FloatTensor(testset).to(device)
+        testlabels = [int(label) for label in testlabels]
+        testlabels = torch.LongTensor(testlabels).to(device)
+
+        # define the architectures we're working with
+        smallnet = ThreeLayerNet(trainset[0].shape[0], (20,20,20))
+        smallnet.to(device)
+        bignet = ThreeLayerNet(trainset[0].shape[0], (1024,1024,1024))
+        bignet.to(device)
+        singlelayer = OneLayerNet(trainset[0].shape[0])
+        singlelayer.to(device)
+        netdict = {smallnet: "20_20_20", bignet: "1024_1024_1024", \
+            singlelayer: "1024"}
+
+        for net in netdict:
+            criterion = nn.CrossEntropyLoss()
+            optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+            for epoch in range(1000):  # loop over the dataset multiple times
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = net(trainset)
+                loss = criterion(outputs, trainlabels)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                outputs = net(testset)
+                _, predicted = torch.max(outputs.data, 1)
+                total += testlabels.size(0)
+                correct += (predicted == testlabels).sum().item()
+
+                logging.info(rep + ',' + netdict[net] + ',' + str(i) + ',' + \
+                    str(100 * correct/total))
---
+++ word_content/kway_construction.py
@@ -0,0 +1,58 @@
+import csv
+import random
+
+verbs = open("../stimuli/clean_verbs.txt", 'r').read().split()
+trainVerbs = verbs[:80]
+testVerbs = verbs[80:100]
+
+## tense
+# present_verbs = open("stimuli/present_verbs.txt", 'r').read().split()
+
+## nouns
+nouns = []
+with open("../stimuli/other_nouns.csv", 'r') as f:
+    reader = csv.reader(f)
+    for i, line in enumerate(reader):
+        nouns.append([line[0], i]) #vocabulary index for one-hot vectors
+
+# simulate train-test split
+trainSubjs = nouns[:20]
+testSubjs = nouns[80:100]
+
+# create train set: every train subject is paired with its vocabulary index
+sens = []
+for trainVerb in trainVerbs:
+    for trainSubj in trainSubjs:
+         for trainObj in trainSubjs:
+            if trainObj != trainSubjs:
+                # each stim has the form (sentence, vocab index)
+                sens.append(("the " + trainSubj[0] + ' ' + trainVerb + " the " + trainObj[0], \
+                    trainSubj[1]))
+
+random.shuffle(sens)
+sens = sens[:4000]
+
+with open("../data/kway_train.csv", 'w') as csv_file:
+     data_writer = csv.writer(csv_file, delimiter = ',')
+     for row in sens:
+         data_writer.writerow(list(row))
+
+
+# create test set
+sens = []
+for testVerb in testVerbs:
+    for testSubj in trainSubjs:
+         for testObj in testSubjs:
+            if testObj != testSubj:
+                # each stim has the form (sentence, vocab index)
+                sens.append(("the " + testSubj[0] + ' ' + testVerb + " the " + testObj[0], \
+                    testSubj[1]))
+
+
+random.shuffle(sens)
+sens = sens[:1000]
+
+with open("../data/kway_test.csv", 'w') as csv_file:
+     data_writer = csv.writer(csv_file, delimiter = ',')
+     for row in sens:
+         data_writer.writerow(list(row))
